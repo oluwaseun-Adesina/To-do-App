ce5ce7231caec76d508f913077d48d0a1c7d1b47
Sarah Ossai <sarahossai6@gmail.com>
2022-12-08 10:24:16 +0100

    Worked on CRUD

--- src/App.js
+++ src/App.js
@@ -1,15 +1,115 @@
 import './App.css';
 import React from 'react';
-
+import { useEffect, useRef } from 'react';
 
 export const App = () => {
+  const ref = useRef(null);
 
+  useEffect(() => {
+    const el2 = ref.current;
+    console.log(el2);
+
+    let form = document.getElementById("form");
+    let textInput = document.getElementById("textInput");
+    let dateInput = document.getElementById("dataInput");
+    let textarea = document.getElementById("textarea")
+    let msg = document.getElementById("msg");
+    let tasks = document.getElementById("tasks");
+    let add = document.getElementById("add");
+
+    //form validation
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+      formValidation();
+    });
+
+    let formValidation = () => {
+      if (textInput.value === ""){
+        console.log("failure");
+        msg.innerHTML = "Task cannot be blank";
+      }
+      else{
+        console.log("success");
+        msg.innerHTML = "";
+        acceptData();
+        add.setAttribute("data-bs-dismiss", "modal");
+        add.click();
+
+        (() => {
+          add.setAttribute("data-bs-dismiss", "");
+        })();
+      }
+    };
+    //collect data
+    let data = [];
+
+    let acceptData = () => {
+      data.push({
+        text: textInput?.value || '',
+        date: dateInput?.value || '',
+        description: textarea?.value || '',
+      });
+
+      localStorage.setItem("data", JSON.stringify(data));
+
+      console.log(data);
+     createTasks();
+    }
+
+    //create new tasks
+    let createTasks = () => {
+      tasks.innerHTML = "";
+      data.map((x, y) => {
+        return (tasks.innerHTML += `
+        <div id=${y}> 
+              <span class="fw-bold">${x.text}</span>
+              <span class="small text-secondary">${x.date}</span>
+              <p>${x.description}</p>
+      
+              <span class="options">
+                <i onClick= "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fa-solid fa-pen-to-square"></i>
+                <i onClick ="deleteTask(this);createTasks()" class="fa-solid fa-trash"></i>
+              </span>
+            </div>
+        `);
+      });
+    
+      resetForm();
+    };
+    let resetForm = () => {
+      textInput.value = "";
+      dateInput.value = "";
+      textarea.value = "";
+    };
+    //delete a task
+    let deleteTask = (e) => {
+      e.parentElement.parentElement.remove();
+    
+      data.splice(e.parentElement.parentElement.id, 1);
+    
+      localStorage.setItem("data", JSON.stringify(data));
+    
+      console.log(data);
+      
+    };
+//edit a task
+let editTask = (e) => {
+  let selectedTask = e.parentElement.parentElement;
+
+  textInput.value = selectedTask.children[0].innerHTML;
+  dateInput.value = selectedTask.children[1].innerHTML;
+  textarea.value = selectedTask.children[2].innerHTML;
+
+  deleteTask(e);
+};
+
+  })
   return (
 
     <React.Fragment>
       <div className='bg-neutral-300 w-full h-screen grid'>
 
-        <div className='bg-white text-neutral-700 m-auto border-4 border-blue-400 w-5/6 md:w-1/4 h-5/6 rounded-lg'>
+        <div className='bg-white text-neutral-700 m-auto border-4 border-blue-400 w-5/6 md:w-1/3 h-5/6 rounded-lg'>
           <div className='px-3 py-3'>
             <p className='font-bold text-3xl'>TODO App</p>
             <br/>
